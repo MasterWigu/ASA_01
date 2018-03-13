8e9cd122c2bd410cf2827eb48ff08d2ea52fd68d
MasterWigu <moreira.oliveira@live.com.pt>
2018-03-13 17:22:56 +0000

    criar ficheiro

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct {
+	
+} ;
+
+void tarjan() {
+	int visited = 0;
+
+
+}
+
+int main(char* argv, int argc) {
+	
+
+
+	return 0;
+}
