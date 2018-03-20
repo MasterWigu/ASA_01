d4cc566e173b532771f48edc4a9bad9d4332911d
Unknown <moreira.oliveira@live.com.pt>
2018-03-20 21:32:25 +0000

    lugklgul

--- main.c
+++ main.c
@@ -11,16 +11,16 @@ extern graph graph1;
 int numComp;
 LLElem* SCC;
 LLElem* L;
-int *aux2; 
+int *adj;
 
 void visit(int u);
 
-int adjMenor(int* a, int i, int j) {
-	if (a[i] < a[j])
+int adjMenor(int i, int j) {
+	if (adj[2*i] < adj[2*j])
 		return 1;
-	if (a[i] > a[j])
+	if (adj[2*i] > adj[2*j])
 		return 0;
-	if (a[i+1] < a[j+1])
+	if (adj[2*i+1] <= adj[2*j+1])
 		return 1;
 	return 0;
 }
@@ -35,27 +35,71 @@ int min(int a, int b) {
 	return b;
 }
 
-void merge(int *a, int l, int m, int r, int* adj) {  
-	int i, j, k; 
-	for (i = m+1; i > l; i--)  
-    	aux2[i-1] = a[i-1]; 
-	for (j = m; j < r; j++) 
-    	aux2[r+m-j] = a[j+1]; 
-  	for (k = l; k <= r; k++) 
-    	if (adjMenor(adj,aux2[j], aux2[i])) 
-      		a[k] = aux2[j--]; 
-    	else 
-      		a[k] = aux2[i++]; 
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r) {
+//VEIO DE https://www.geeksforgeeks.org/merge-sort/
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (adjMenor(L[i], R[j])) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
 }
 
-void mergesort(int *a, int l, int r, int* adj) {  
-	int m = (r+l)/2;
-	if (r <= l) 
-		return; 
-	mergesort(a, l, m); 
-	mergesort(a, m+1, r); 
-	merge(a, l, m, r, adj); 
-}
 
 void tarjan() {
 	int i;
@@ -105,7 +149,7 @@ void visit(int u) {
 	}
 }
 
-void printAdjComp(int* adj) {
+void printAdjComp() {
 	int N = adj[1];
 	int i;
 	int in=0;
@@ -120,29 +164,33 @@ void printAdjComp(int* adj) {
 	}
 
 
-	int aux[N];
+	int ind[N];
 	for (i=0; i<N; i++)
-		aux[i]=i;
-	//ordenar adj
+		ind[i]=i+1;
 
-	aux2 = (int*) malloc(N*sizeof(int));
-	mergesort(aux, 0, N, adj+2);
 
-	for (i=2; i<(2*N)+2; i+=2) {
-		if (graph1.v[adj[i]-1].low != graph1.v[adj[i+1]-1].low)
+	mergeSort(ind, 0, N-1);
+/*
+	printf("\n");
+	for (i=0; i<N; i++)
+		printf("%d ", ind[i]);
+	printf("\n");
+*/
+	for (i=0; i<N; i++) {
+		if (graph1.v[adj[2*ind[i]]-1].low != graph1.v[adj[2*ind[i]+1]-1].low)
 			numLigComp++;
 	}
 
 	int comps2[2*numLigComp];
-	for (i=2; i<(2*N)+2; i+=2) {
-		if (graph1.v[adj[i]-1].low != graph1.v[adj[i+1]-1].low) {
+	for (i=0; i<N; i++) {
+		if (graph1.v[adj[2*ind[i]]-1].low != graph1.v[adj[2*ind[i]+1]-1].low) {
 			if (in==0 || 
-				comps2[in-2] != comps[graph1.v[adj[i]-1].numSCC]+1 || 
-				comps2[in-1] != comps[graph1.v[adj[i+1]-1].numSCC]+1) {
+				comps2[in-2] != comps[graph1.v[adj[2*ind[i]]-1].numSCC]+1 || 
+				comps2[in-1] != comps[graph1.v[adj[2*ind[i]+1]-1].numSCC]+1) {
 				//printf("A I=%d  IN=%d  ADJ=%d  ADJ2=%d\n", i, in, adj[i], adj[i+1]);
 
-				comps2[in] = comps[graph1.v[adj[i]-1].numSCC]+1;
-				comps2[in+1] = comps[graph1.v[adj[i+1]-1].numSCC]+1;
+				comps2[in] = comps[graph1.v[adj[2*ind[i]]-1].numSCC]+1;
+				comps2[in+1] = comps[graph1.v[adj[2*ind[i]+1]-1].numSCC]+1;
 				//printf("B I=%d  IN=%d  ADJ=%d  ADJ2=%d\n", i, in, adj[i], adj[i+1]);
 				in+=2;
 			}
@@ -175,13 +223,12 @@ int* lerFich() {
 }
 
 int main(int argc, char** argv) {
-	int* nums;
 	numComp = 0;
-	nums = lerFich();
-	createGraph(nums);
+	adj = lerFich();
+	createGraph(adj);
 
 	tarjan();
-	printAdjComp(nums);
+	printAdjComp();
 	//printGraph();
 	return 0;
 }
